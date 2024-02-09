81ff524fe0faae688c220e658a3691008a528542
blackieghoster <blackieghoster@gmail.com>
2024-02-09 21:13:35 +0100

    Final fix

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
     path('all', views.all_users, name='all_users'),
-    path('add_user', views.add_user, name='add_user')
+    path('add_user', views.add_user, name='add_user'),
+    path('add_user_post', views.add_user_post, name='add_user_post')
+
 ]
