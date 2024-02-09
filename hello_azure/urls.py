b8cd52d0fcf9871e033469610b408ae3be7ba597
blackieghoster <blackieghoster@gmail.com>
2024-02-09 21:35:14 +0100

    Added deletion

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('hello', views.hello, name='hello'),
     path('all', views.all_users, name='all_users'),
     path('add_user', views.add_user, name='add_user'),
-    path('add_user_post', views.add_user_post, name='add_user_post')
+    path('add_user_post', views.add_user_post, name='add_user_post'),
+    path('delete_user', views.delete_user, name='delete_user'),
+    path('delete_user_post', views.delete_user_post, name='delete_user_post')
 
 ]
